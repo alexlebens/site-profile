e985f905f2696ede4cb962325f109ba2559f3dfd
Alex Lebens <alexanderlebens@gmail.com>
2025-07-15 19:13:47 -0500

    formatting changes

--- src/components/DynamicIcon.tsx
+++ src/components/DynamicIcon.tsx
@@ -4,10 +4,12 @@ import * as MdIcons from 'react-icons/md';
 import * as AiIcons from 'react-icons/ai';
 import * as GiIcons from 'react-icons/gi';
 import * as IoIcons from 'react-icons/io';
-import * as CiIcons from "react-icons/ci";
-import * as FiIcons from "react-icons/fi";
-import * as LuIcons from "react-icons/lu";
+import * as CiIcons from 'react-icons/ci';
+import * as FiIcons from 'react-icons/fi';
+import * as LuIcons from 'react-icons/lu';
 import * as SiIcons from 'react-icons/si';
+
+// DynamicIcon.tsx - Load React Icon library dynamically from attributes from Directus
 
 const iconSets = {
   fa: FaIcons,
@@ -21,13 +23,25 @@ const iconSets = {
   si: SiIcons,
 };
 
-const DynamicIcon = ({ name, set = 'fa', size = 20, color = 'currentColor', className = '' }: {name: string, set: string, size: number, color: string, className: string }) => {
+const DynamicIcon = ({
+  name,
+  set = 'fa',
+  size = 20,
+  color = 'currentColor',
+  className = '',
+}: {
+  name: string;
+  set: string;
+  size: number;
+  color: string;
+  className: string;
+}) => {
   let IconComponent = FaIcons.FaAlignCenter;
 
-  if (name.startsWith("Fa")) {
-    IconComponent = iconSets["fa"][name]
-  } else if (name.startsWith("Si")) {
-    IconComponent = iconSets["si"][name]
+  if (name.startsWith('Fa')) {
+    IconComponent = iconSets['fa'][name];
+  } else if (name.startsWith('Si')) {
+    IconComponent = iconSets['si'][name];
   } else {
     IconComponent = iconSets[set][name];
   }
