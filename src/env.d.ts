b9d85a552002ffbe447c3452be5147f2f1117fe8
Alex Lebens <alexanderlebens@gmail.com>
2025-08-11 19:10:46 -0500

    fix layout

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 /// <reference types="astro/content" />
-=======
-/// <reference types="astro/client" />
-/// <reference path="../.astro/types.d.ts" />
->>>>>>> 184f0c7 (fix path)
