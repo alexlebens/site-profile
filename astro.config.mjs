04344808bdd9a22f3c8ac4c21dec16e71b398b59
Alex Lebens <alexanderlebens@gmail.com>
2026-03-11 21:50:38 -0500

    feat: enable security feature

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,6 +13,8 @@ import { getSiteURL } from './src/support/url';
 export default defineConfig({
   site: getSiteURL(),
 
+  security: { csp: true },
+
   image: {
     remotePatterns: [
       { protocol: 'https', hostname: '*.alexlebens.net' },
